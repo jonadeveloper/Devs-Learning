70a236261e755d4a59abd26873da16801d5f064e
Simon Navarrete <66700738+GoldZy11@users.noreply.github.com>
2023-02-03 20:27:21 -0300

    save

--- client/devs-learning/src/router/index.tsx
+++ client/devs-learning/src/router/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../views/Home'
+import { Test } from '../views/Test'
 
 export const AppRouter = () => {
     return (
         <div>
             <Routes>
                 <Route path={`/`} element={<Home />} />
+                <Route path={`/test`} element={<Test />} />
             </Routes>
         </div>
     )
