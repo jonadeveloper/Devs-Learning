fac35a097198187e41eff1811dac1c42cad401af
Jonathan Kim <jonnykimbk@hotmail.com>
2023-02-04 20:39:56 -0300

    Searchbar/animacion

--- client/devs-learning/src/components/searchbar/searchbar.tsx
+++ client/devs-learning/src/components/searchbar/searchbar.tsx
@@ -1,10 +1,20 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useRef } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
+import Collapse from "@mui/material/Collapse";
+
+import { Box } from "@mui/system";
 
 export default function SearchBar() {
   const [search, setSearch] = useState("");
+  const [checked, setChecked] = useState(false);
+
+  const checkClick = () => {
+    if (search == "") {
+      setChecked((prev) => !prev);
+    }
+  };
 
   const handleChange = (
     event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
@@ -20,20 +30,23 @@ export default function SearchBar() {
 
   return (
     <div className="searchBar">
-      searchbar
       <form onSubmit={handleSubmit}>
-        <TextField
-          id="outlined-basic"
-          label="Search..."
-          variant="outlined"
-          type="text"
-          value={search}
-          onChange={(event) => handleChange(event)}
-        />
+        <Box sx={{ display: "flex" }}>
+          <Collapse orientation="horizontal" in={checked}>
+            <TextField
+              id="outlined-basic"
+              label="Search..."
+              variant="outlined"
+              type="text"
+              value={search}
+              onChange={(event) => handleChange(event)}
+            />
+          </Collapse>
 
-        <IconButton type="submit">
-          <SearchIcon />
-        </IconButton>
+          <IconButton type="submit" onClick={checkClick}>
+            <SearchIcon sx={{ display: "flex" }} />
+          </IconButton>
+        </Box>
       </form>
     </div>
   );
