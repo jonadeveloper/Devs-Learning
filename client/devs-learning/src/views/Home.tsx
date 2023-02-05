1f904075d733b79a601b9e862a13ed92fd302aee
Jonathan Kim <jonnykimbk@hotmail.com>
2023-02-05 20:14:51 -0300

    resolver problema

--- client/devs-learning/src/views/Home.tsx
+++ client/devs-learning/src/views/Home.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Box } from '@mui/system'
-import { CardList } from '../components/Cards/CardList'
-
+import React from "react";
+import { Box } from "@mui/system";
+import { CardList } from "../components/Cards/CardList";
 
 export const Home = () => {
-    return (
-        <div>
-            <Box>
-                <CardList />
-            </Box>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Box>
+        <CardList />
+      </Box>
+    </div>
+  );
+};
