69befa1daa40cdb8b104163fd6a5f8c3fc926577
AdicTX <fedealdama@hotmail.com>
2023-02-06 16:58:55 -0300

    Add NavBar
    Add searchBar into NavBar

--- client/devs-learning/src/views/Home.tsx
+++ client/devs-learning/src/views/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box } from "@mui/system";
 import { CardList } from "../components/Cards/CardList";
-
+import NavBar from "../components/navBar/NavBar";
 export const Home = () => {
   return (
     <div>
       <Box>
+        <NavBar />
         <CardList />
       </Box>
     </div>
