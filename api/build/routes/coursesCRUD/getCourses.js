81d700d61238b071c609959cb4c32bd3db665fba
Simon Navarrete <66700738+GoldZy11@users.noreply.github.com>
2023-02-07 16:05:39 -0300

    adapt getCourses function to get all

--- api/build/routes/coursesCRUD/getCourses.js
+++ api/build/routes/coursesCRUD/getCourses.js
@@ -14,19 +14,26 @@ const { Course, Category } = require('../../db');
 function getCourses(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            let { name } = req.params;
-            name = name.split(" ").join("-").toLowerCase();
-            let course = yield Course.findAll({
-                where: { "name": name },
-                include: {
-                    model: Category,
-                    attributes: ['name'],
-                    through: {
-                        attributes: []
+            let { name } = req.query;
+            console.log(name);
+            if (name) {
+                name = name.split(" ").join("-").toLowerCase();
+                let course = yield Course.findAll({
+                    where: { "name": name },
+                    include: {
+                        model: Category,
+                        attributes: ['name'],
+                        through: {
+                            attributes: []
+                        }
                     }
-                }
-            });
-            return res.status(200).send(course);
+                });
+                return res.status(200).send(course);
+            }
+            else {
+                let course = yield Course.findAll({});
+                return res.status(200).send(course);
+            }
         }
         catch (err) {
             return res.status(404).send(err);
--- api/src/routes/coursesCRUD/getCourses.ts
+++ api/src/routes/coursesCRUD/getCourses.ts
@@ -1,20 +1,27 @@
-const {Course, Category} = require('../../db');
+const { Course, Category } = require('../../db');
 
-export async function getCourses(req: any, res: any){
+export async function getCourses(req: any, res: any) {
+
     try {
-        let { name } = req.params;
-        name = name.split(" ").join("-").toLowerCase();
-        let course = await Course.findAll({
-            where: {"name": name},
-            include: {
-                model: Category,
-                attributes: ['name'],
-                through: {
-                    attributes: []
+        let { name } = req.query;
+        if (name) {
+            name = name.split(" ").join("-").toLowerCase();
+            let course = await Course.findAll({
+                where: { "name": name },
+                include: {
+                    model: Category,
+                    attributes: ['name'],
+                    through: {
+                        attributes: []
+                    }
                 }
-            }
-        });
-        return res.status(200).send(course);
+            });
+            return res.status(200).send(course);
+        }
+        else {
+            let course = await Course.findAll({});
+            return res.status(200).send(course);
+        }
     } catch (err) {
         return res.status(404).send(err);
     }
