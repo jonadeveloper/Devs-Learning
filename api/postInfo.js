e73488cbdc69d6fb26495e10e08031f87d06d190
unknown <estiven.carmona@udea.edu.co>
2023-02-07 10:11:17 -0500

    Función creadora de cursos

---
+++ api/postInfo.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+module.exports.postInfo = async function (course) {
+    let courseDB = {
+        name: course.name,
+        level: course.level,
+        description: course.description,
+        price: course.price,
+        category: course.category
+    };
+    await axios.post("http://localhost:3001/courses", courseDB).then(function (response) {
+        console.log(response);
+    }).catch(function (error) {
+        console.log(error);
+    });
+};
+
