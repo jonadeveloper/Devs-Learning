98dc06e894717430d5c4b83de0e6e37570b6b41c
unknown <estiven.carmona@udea.edu.co>
2023-02-07 10:11:17 -0500

    Función creadora de cursos

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 const server = require('./build/app.js');
 const { conn } = require('./build/db.js');
+const { postInfo } = require('./postInfo.js');
+const dbInfo = require('./db.json');
 
 const PORT = 3001;
 
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
-    console.log('%s listening at', PORT); 
+    console.log('%s listening at', PORT);
+    dbInfo.courses.map((course) => {
+      return postInfo(course);
+    });
   });
 });
+
