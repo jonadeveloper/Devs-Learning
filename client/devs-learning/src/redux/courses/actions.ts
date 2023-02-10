9032cb73b5b4ec650bc5d54e198092973a115389
Jonathan Kim <jonnykimbk@hotmail.com>
2023-02-10 19:26:20 -0300

    correccion de conflictos

--- client/devs-learning/src/redux/courses/actions.ts
+++ client/devs-learning/src/redux/courses/actions.ts
@@ -20,36 +20,36 @@ export const getCourses = (): ThunkAction<
   };
 };
 
-
 export const searchCourses = (
-    courses: Array<CoursoBack>,
-    search: string
-  ): ThunkAction<void, RootState, unknown, AnyAction> => {
-    return (dispatch) => {
-      try {
-        let allcourses = [...courses];
-  
-        if (search !== "") {
-          search = search.toLowerCase();
-  
-          allcourses = allcourses.filter((course) => {
-            if (course.name.toLowerCase().includes(search)) return true;
-          });
-        }
-  
-        dispatch(reducer.searched(allcourses));
-      } catch (error) {
-        console.log("no se encontro el curso buscado, se muestran todos");
-  
-        dispatch(reducer.searched(error));
-      }
-    };
-  };
+  courses: Array<CoursoBack>,
+  search: string
+): ThunkAction<void, RootState, unknown, AnyAction> => {
+  return (dispatch) => {
+    try {
+      let allcourses = [...courses];
 
-  export const setCurrentCourse = (card: CoursoBack): ThunkAction<void, RootState, unknown, AnyAction> => {
-    return (dispatch) => {
-        console.log(card)
-       return dispatch(reducer.currentCourse(card))
+      if (search !== "") {
+        search = search.toLowerCase();
+
+        allcourses = allcourses.filter((course) => {
+          if (course.name.toLowerCase().includes(search)) return true;
+        });
+      }
+
+      dispatch(reducer.searched(allcourses));
+    } catch (error) {
+      console.log("no se encontro el curso buscado, se muestran todos");
+
+      dispatch(reducer.searched(error));
     }
   };
+};
 
+export const setCurrentCourse = (
+  card: CoursoBack
+): ThunkAction<void, RootState, unknown, AnyAction> => {
+  return (dispatch) => {
+    console.log(card);
+    return dispatch(reducer.currentCourse(card));
+  };
+};
--- client/devs-learning/src/redux/courses/slice.ts
+++ client/devs-learning/src/redux/courses/slice.ts
@@ -2,41 +2,41 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Course, CoursoBack } from "../../components/Cards/Card";
 
 interface CoursesState {
-    courses: CoursoBack[];
-    coursesFiltered: CoursoBack[];
-    categories: Array<unknown>;
-    currentPage: number;
-    currentCourse: CoursoBack
+  courses: CoursoBack[];
+  coursesFiltered: CoursoBack[];
+  categories: Array<unknown>;
+  currentPage: number;
+  currentCourse: CoursoBack;
 }
 
 const initialState: CoursesState = {
-    courses: [],
-    coursesFiltered: [],
-    categories: [],
-    currentCourse: {
-        description: '',
-        id: '',
-        level: '',
-        name: '',
-        price: '',
-    },
-    currentPage: 1
-}
+  courses: [],
+  coursesFiltered: [],
+  categories: [],
+  currentCourse: {
+    description: "",
+    id: "",
+    level: "",
+    name: "",
+    price: "",
+  },
+  currentPage: 1,
+};
 
 export const courses = createSlice({
-    name: 'courses',
-    initialState, // defino initial state (state= initialState)
-    reducers: {
-        allCourses: (state, { payload }) => {
-            state.courses = payload;
-        },
-        currentCourse: (state, action: PayloadAction<CoursoBack>) => {
-            state.currentCourse = action.payload;
-        },
-        searched: (state, { payload }) => {
-            state.coursesFiltered = payload;
-          },
-    }
-})
+  name: "courses",
+  initialState, // defino initial state (state= initialState)
+  reducers: {
+    allCourses: (state, { payload }) => {
+      state.courses = payload;
+    },
+    currentCourse: (state, action: PayloadAction<CoursoBack>) => {
+      state.currentCourse = action.payload;
+    },
+    searched: (state, { payload }) => {
+      state.coursesFiltered = payload;
+    },
+  },
+});
 
 export const reducer = courses.actions;
