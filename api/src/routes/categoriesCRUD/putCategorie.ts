6a355286bf32dcb14c37bbfaaee727365058c531
Jonathan Kim <jonnykimbk@hotmail.com>
2023-02-10 19:56:10 -0300

    corrigiendo conflicto con database

--- api/src/routes/categoriesCRUD/putCategorie.ts
+++ api/src/routes/categoriesCRUD/putCategorie.ts
@@ -1,7 +1,7 @@
-export async function putCategorie (_req: any, res: any) {
-    try {
-        return res.status(200).send("Put Category");
-    } catch (err) {
-        return res.status(404).send(err);
-    }
+export async function putCategorie(_req: any, res: any) {
+  try {
+    return res.status(200).send("Put Category");
+  } catch (err) {
+    return res.status(404).send(err);
+  }
 }
--- api/src/routes/coursesCRUD/getCourses.ts
+++ api/src/routes/coursesCRUD/getCourses.ts
@@ -1,36 +1,34 @@
-const { Course, Category } = require('../../db');
+const { Course, Category } = require("../../db");
 
 export async function getCourses(req: any, res: any) {
-
-    try {
-        let { name } = req.query;
-        if (name) {
-            name = name.split(" ").join("-").toLowerCase();
-            let course = await Course.findAll({
-                where: { "name": name },
-                include: {
-                    model: Category,
-                    attributes: ['name'],
-                    through: {
-                        attributes: []
-                    }
-                }
-            });
-            return res.status(200).send(course);
-        }
-        else {
-            let course = await Course.findAll({
-                includes : {
-                    Category,
-                    attributes: ['name'],
-                    through: {
-                        attributes: []
-                    }
-                }
-            });
-            return res.status(200).send(course);
-        }
-    } catch (err) {
-        return res.status(404).send(err);
+  try {
+    let { name } = req.query;
+    if (name) {
+      name = name.split(" ").join("-").toLowerCase();
+      let course = await Course.findAll({
+        where: { name: name },
+        include: {
+          model: Category,
+          attributes: ["name"],
+          through: {
+            attributes: [],
+          },
+        },
+      });
+      return res.status(200).send(course);
+    } else {
+      let course = await Course.findAll({
+        includes: {
+          Category,
+          attributes: ["name"],
+          through: {
+            attributes: [],
+          },
+        },
+      });
+      return res.status(200).send(course);
     }
+  } catch (err) {
+    return res.status(404).send(err);
+  }
 }
