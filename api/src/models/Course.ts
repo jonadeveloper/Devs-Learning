ebc007de8665eb25e76f18334490e7086f327bc2
= <=>
2023-02-10 13:45:28 -0300

    course actualizado

--- api/src/models/Course.ts
+++ api/src/models/Course.ts
@@ -12,24 +12,18 @@ module.exports = (sequelize: any) => {
             type: DataTypes.STRING,
             allowNull: false
         },
-<<<<<<< HEAD
         img: {
             type: DataTypes.STRING,
             allowNull: false
         },
         level: {
             type: DataTypes.ENUM("beginner", "intermediate", "advanced"),
-=======
-        level: {
-            type: DataTypes.ENUM("beginner","intermediate","advanced"),
->>>>>>> development
             allowNull: false
         },
         description: {
             type: DataTypes.STRING,
             allowNull: false
         },
-<<<<<<< HEAD
         descriptionComplete: {
             type: DataTypes.TEXT,
             allowNull: false
@@ -42,41 +36,9 @@ module.exports = (sequelize: any) => {
             type: DataTypes.STRING,
             allowNull: false
         },
-=======
->>>>>>> development
         price: {
             type: DataTypes.STRING,
             allowNull: false
         }
-<<<<<<< HEAD
     })
 }
-=======
-    })}
->>>>>>> development
-
-module.exports = (sequelize: any) => {
-    sequelize.define('course', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull: false,
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        level: {
-            type: DataTypes.ENUM("beginner","intermediate","advanced"),
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        price: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    })}
