6aca9103fb9e6b9b8ef7ee3aa2abef281d8b231b
Jonathan Kim <jonnykimbk@hotmail.com>
2023-02-10 19:30:21 -0300

    correcion de conflictos/merge

--- client/devs-learning/src/components/Cards/Card.tsx
+++ client/devs-learning/src/components/Cards/Card.tsx
@@ -1,87 +1,122 @@
-import Card from '@mui/material/Card'
-import CardActionArea from '@mui/material/CardActionArea'
-import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
-import Chip from '@mui/material/Chip'
-import Divider from '@mui/material/Divider'
-import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
-import { Box } from '@mui/system'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { setCurrentCourse } from '../../redux/courses/actions'
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
+import React from "react";
+import { Link } from "react-router-dom";
+import { setCurrentCourse } from "../../redux/courses/actions";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooksRedux";
 
 export interface Course {
-    categoria: number
-    nombre: string
-    image: string
-    summary: string
-    duracion: number
-    precio: number
-    idProfesor: number
-    dificultad: string
+  categoria: number;
+  nombre: string;
+  image: string;
+  summary: string;
+  duracion: number;
+  precio: number;
+  idProfesor: number;
+  dificultad: string;
 }
 export interface CoursoBack {
-    description: string
-    id: string
-    level: string
-    name: string
-    price: string
+  description: string;
+  id: string;
+  level: string;
+  name: string;
+  price: string;
 }
 interface Props {
-    card: CoursoBack
-    index: number
+  card: CoursoBack;
+  index: number;
 }
 
 export const CardComponent = ({ card, index }: Props) => {
-    const PATH : string = `/courseDetail/${card.id}`;
-    const dispatch = useAppDispatch();
+  const PATH: string = `/courseDetail/${card.id}`;
+  const dispatch = useAppDispatch();
 
-    return (
-        <Grid key={index} item xl={3} lg={4} sx={{ display: 'flex', justifyContent: 'center' }} >
-            <Link style={{textDecoration: 'none'}} to = {PATH} onClick={() => dispatch(setCurrentCourse(card))}>
-            <Card sx={{ maxWidth: 345, minHeight: 500, borderRadius: 2, display: 'flex' }}>
-                <CardActionArea sx={{ display: 'grid', gridTemplateRows: 'auto 1fr' }}>
+  return (
+    <Grid
+      key={index}
+      item
+      xl={3}
+      lg={4}
+      sx={{ display: "flex", justifyContent: "center" }}
+    >
+      <Link
+        style={{ textDecoration: "none" }}
+        to={PATH}
+        onClick={() => dispatch(setCurrentCourse(card))}
+      >
+        <Card
+          sx={{
+            maxWidth: 345,
+            minHeight: 500,
+            borderRadius: 2,
+            display: "flex",
+          }}
+        >
+          <CardActionArea
+            sx={{ display: "grid", gridTemplateRows: "auto 1fr" }}
+          >
+            {/* Course Name */}
+            <CardMedia
+              component="img"
+              height="200"
+              alt="Course Name"
+              // image={card.image}
+            />
+            <CardContent sx={{ justifySelf: "flex-start" }}>
+              <Box>
+                {/* Course Title */}
+                <Typography
+                  gutterBottom
+                  variant="h3"
+                  component="div"
+                  sx={{ fontSize: 30, fontWeight: 700 }}
+                >
+                  {card.name} {index}
+                </Typography>
+                {/* Course Price */}
+                <Typography
+                  gutterBottom
+                  variant="h4"
+                  component="div"
+                  sx={{ fontSize: 25, fontWeight: 700 }}
+                >
+                  ${card.price}
+                </Typography>
+                {/* Category */}
+                <Box my={2}>
+                  <Chip
+                    label="Development"
+                    sx={{ backgroundColor: "greenyellow" }}
+                  />
+                </Box>
+                {/* Course sumary */}
+                <Typography variant="body2" color="text.secondary">
+                  {card.description}
+                </Typography>
+              </Box>
+              <Divider sx={{ my: 2 }} />
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                {/* <Typography sx={{ display: 'flex', fontWeight: "600", gap: 1 }}>Duration: <span style={{ color: "grey" }}> {card.duracion} hours</span></Typography> */}
+                <Typography sx={{ display: "flex", fontWeight: "600", gap: 1 }}>
+                  Duration: <span style={{ color: "grey" }}> 1 hours</span>
+                </Typography>
 
-                    {/* Course Name */}
-                    <CardMedia
-                        component="img"
-                        height="200"
-                        alt="Course Name"
-                    // image={card.image}
-                    />
-                    <CardContent sx={{ justifySelf: 'flex-start' }}>
-                        <Box>
-                            {/* Course Title */}
-                            <Typography gutterBottom variant="h3" component="div" sx={{ fontSize: 30, fontWeight: 700 }}>
-                                {card.name} {index}
-                            </Typography>
-                            {/* Course Price */}
-                            <Typography gutterBottom variant="h4" component="div" sx={{ fontSize: 25, fontWeight: 700 }}>
-                                ${card.price}
-                            </Typography>
-                            {/* Category */}
-                            <Box my={2}>
-                                <Chip label="Development" sx={{ backgroundColor: "greenyellow" }} />
-                            </Box>
-                            {/* Course sumary */}
-                            <Typography variant="body2" color="text.secondary">
-                                {card.description}
-                            </Typography>
-                        </Box>
-                        <Divider sx={{ my: 2 }} />
-                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                            {/* <Typography sx={{ display: 'flex', fontWeight: "600", gap: 1 }}>Duration: <span style={{ color: "grey" }}> {card.duracion} hours</span></Typography> */}
-                            <Typography sx={{ display: 'flex', fontWeight: "600", gap: 1 }}>Duration: <span style={{ color: "grey" }}> 1 hours</span></Typography>
-
-                            <Typography sx={{ display: 'flex', fontWeight: "600", gap: 1 }}>Difficulty: <span style={{ color: "grey" }}> {card.level}</span></Typography>
-                        </Box>
-
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-            </Link>
-        </Grid>
-    )
-}
+                <Typography sx={{ display: "flex", fontWeight: "600", gap: 1 }}>
+                  Difficulty:{" "}
+                  <span style={{ color: "grey" }}> {card.level}</span>
+                </Typography>
+              </Box>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Link>
+    </Grid>
+  );
+};
--- client/devs-learning/src/redux/courses/actions.ts
+++ client/devs-learning/src/redux/courses/actions.ts
@@ -2,7 +2,7 @@ import { AnyAction, ThunkAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import { CoursoBack } from "../../components/Cards/Card";
 import { RootState } from "../store";
-import { reducer } from "./slice"
+import { reducer } from "./slice";
 
 const BACK = "http://181.127.189.247:3001";
 
@@ -20,35 +20,36 @@ export const getCourses = (): ThunkAction<
   };
 };
 
-
 export const searchCourses = (
-    courses: Array<CoursoBack>,
-    search: string
-  ): ThunkAction<void, RootState, unknown, AnyAction> => {
-    return (dispatch) => {
-      try {
-        let allcourses = [...courses];
-  
-        if (search !== "") {
-          search = search.toLowerCase();
-  
-          allcourses = allcourses.filter((course) => {
-            if (course.name.toLowerCase().includes(search)) return true;
-          });
-        }
-  
-        dispatch(reducer.searched(allcourses));
-      } catch (error) {
-        console.log("no se encontro el curso buscado, se muestran todos");
-  
-        dispatch(reducer.searched(error));
-      }
-    };
-  };
+  courses: Array<CoursoBack>,
+  search: string
+): ThunkAction<void, RootState, unknown, AnyAction> => {
+  return (dispatch) => {
+    try {
+      let allcourses = [...courses];
 
-  export const setCurrentCourse = (card: CoursoBack): ThunkAction<void, RootState, unknown, AnyAction> => {
-    return (dispatch) => {
-        console.log(card)
-       return dispatch(reducer.currentCourse(card))
+      if (search !== "") {
+        search = search.toLowerCase();
+
+        allcourses = allcourses.filter((course) => {
+          if (course.name.toLowerCase().includes(search)) return true;
+        });
+      }
+
+      dispatch(reducer.searched(allcourses));
+    } catch (error) {
+      console.log("no se encontro el curso buscado, se muestran todos");
+
+      dispatch(reducer.searched(error));
     }
-}
+  };
+};
+
+export const setCurrentCourse = (
+  card: CoursoBack
+): ThunkAction<void, RootState, unknown, AnyAction> => {
+  return (dispatch) => {
+    console.log(card);
+    return dispatch(reducer.currentCourse(card));
+  };
+};
