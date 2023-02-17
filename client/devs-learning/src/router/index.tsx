8b5070af2756f5b96071b3d1a78570069af9b5ce
= <=>
2023-02-17 01:54:11 -0300

    seguimos con el dashboard y añadiendo componentes

--- client/devs-learning/src/router/index.tsx
+++ client/devs-learning/src/router/index.tsx
@@ -14,7 +14,6 @@ import { AuthRouter } from "./AuthRoute";
 import Footer from "../components/Footer/Footer";
 import LandingPage from "../components/Landing/LandingPage";
 import DashboardAdmin from "../components/Dashboards/DashboardAdmin";
-import Admin from "../components/Dashboards/NavBarAdmin"
 
 export const AppRouter = () => {
   const dispatch = useAppDispatch();
@@ -34,7 +33,6 @@ export const AppRouter = () => {
         <Route path={`/categories`} element={<Categories />} />
         <Route path={`/categories/:name`} element={<CoursePerCategories />} />
         <Route path={`/dash/Admin`} element={<DashboardAdmin />} />
-        <Route path={'/admin'} element={<Admin/>}/>
         
 
         <Route
