22697ea42a1a0bff7f4dd834f93ce90b853efe91
riverofrancisco <francisco.rivero124@gmail.com>
2023-02-18 18:09:35 -0300

    agrego el borrador de la tabla de cursos en perfil de usuario

--- client/devs-learning/src/components/Dashboards/User/UserCourses.tsx
+++ client/devs-learning/src/components/Dashboards/User/UserCourses.tsx
@@ -1,21 +1,79 @@
-import React from "react";
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../../../hooks/hooksRedux";
-import { Link as ReactLink } from "react-router-dom";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
+import Rating from "@mui/material/Rating";
+import ChatRoundedIcon from "@mui/icons-material/ChatRounded";
+import IconButton from "@mui/material/IconButton";
 
-const UserCourses: React.FC = () => {
+function createData(
+  name: string,
+  level: string,
+  duration: number,
+  instructor: string,
+  price: number
+) {
+  return { name, level, duration, instructor, price };
+}
+
+const rows = [
+  createData("Curso 1", "high", 24, "Emiliano", 4500),
+  createData("Curso 2", "beginner", 5, "Fran", 2750),
+  createData("Curso 3", "intermediate", 14, "Jonathan", 12000),
+];
+
+export default function BasicTable() {
+  const [value, setValue] = React.useState<number | null>(2);
+
   return (
-    <div>
-      <Typography variant="h3">Your Courses</Typography>
-      ACÁ SE RENDERIZAN LOS CURSOS
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Cursos</TableCell>
+            <TableCell align="left">Nivel</TableCell>
+            <TableCell align="center">Duracion</TableCell>
+            <TableCell align="center">Instructor</TableCell>
+            <TableCell align="right">Precio</TableCell>
+            <TableCell align="center">Rating and Comment</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="left">{row.level}</TableCell>
+              <TableCell align="center">{row.duration}</TableCell>
+              <TableCell align="center">{row.instructor}</TableCell>
+              <TableCell align="right">{row.price}</TableCell>
+              <TableCell align="center">
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Rating
+                    name="simple-controlled"
+                    value={value}
+                    onChange={(event, newValue) => {
+                      setValue(newValue);
+                    }}
+                  />
+                  <IconButton>
+                    <ChatRoundedIcon color="primary" />
+                  </IconButton>
+                </Box>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-};
-
-export default UserCourses;
+}
