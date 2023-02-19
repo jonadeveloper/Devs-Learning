bf532a847c39e61563ecccfd9d017d6be5ffe81e
= <=>
2023-02-19 04:03:54 -0300

    iniciando panel de cursos en dashborad admin

--- client/devs-learning/src/components/Dashboards/Admin/CoursesPanel.tsx
+++ client/devs-learning/src/components/Dashboards/Admin/CoursesPanel.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../hooks/hooksRedux";
 import { Link as ReactLink } from "react-router-dom";
+import { Table } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
@@ -11,13 +12,26 @@ import Stack from "@mui/material/Stack";
 
 const CoursesPanel: React.FC = () => {
   const dispatch = useAppDispatch();
-
+  const column = [
+    { 
+      title:"Course",
+      id: "id"
+    },
+    {
+      title:"Category",
+      id:"id"
+    },
+  ]
   useEffect(() => {});
   return (
-    <div>
-      <Typography variant="h3">Courses</Typography>
-      ACÁ SE RENDERIZA LA INFORMACION SOBRE CURSOS
-    </div>
+    <Grid container xs={12}>
+      <Box width="100%">
+      <Typography textAlign={"center"} variant="h3">Courses</Typography>
+      <Typography textAlign={"center"} variant="h6" mt={3}>
+        In this section we manage all the courses on the platform
+      </Typography>
+      </Box>
+    </Grid>
   );
 };
 
