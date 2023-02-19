57b53e70d2086df802771ad6db13bb58113bf25a
Jonathan Kim <jonnykimbk@hotmail.com>
2023-02-19 13:13:11 -0300

    boton mercadopago 1

--- client/devs-learning/src/components/Cart/Cart.tsx
+++ client/devs-learning/src/components/Cart/Cart.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Box, Divider, Drawer, IconButton, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import { CardComponent } from "../Cards/Card";
 import { useAppSelector } from "../../hooks/hooksRedux";
 import { CartCard } from "./CartCard";
 import { setItem } from "../../utils/localStorage";
+import MeliButton from "../meliButton/meliButton";
 
 interface CartComponentProps {
   open: boolean;
@@ -28,19 +36,29 @@ export interface CoursoBack {
   img: string;
 }
 
-export const CartComponent: React.FC<CartComponentProps> = ({ open, handleStateViewDrawer }) => {
+export const CartComponent: React.FC<CartComponentProps> = ({
+  open,
+  handleStateViewDrawer,
+}) => {
   const { cart } = useAppSelector((state) => state.courses);
 
   React.useEffect(() => {
     setItem("cart", cart);
   }, [cart]);
 
-  const totalPrecio = cart.reduce((acumulado, curso) => acumulado + parseFloat(curso.price), 0);
+  const totalPrecio = cart.reduce(
+    (acumulado, curso) => acumulado + parseFloat(curso.price),
+    0
+  );
 
   return (
     <Drawer anchor={"right"} open={open}>
       <Box sx={{ width: "25em", p: 2 }}>
-        <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
           <Typography variant="h5">Cart</Typography>
           <IconButton color="secondary" onClick={() => handleStateViewDrawer()}>
             <CloseRoundedIcon />
@@ -48,13 +66,17 @@ export const CartComponent: React.FC<CartComponentProps> = ({ open, handleStateV
         </Stack>
         <Divider sx={{ my: 1.5 }} />
         {cart.length > 0
-          ? cart.map((card, index) => <CartCard key={index} card={card} index={index} />)
+          ? cart.map((card, index) => (
+              <CartCard key={index} card={card} index={index} />
+            ))
           : "Nada por aqui"}
         <Box color="secondary">
           <Typography color="secondary" variant="h5">
             Total Price: {totalPrecio}
           </Typography>
         </Box>
+
+        <MeliButton />
       </Box>
     </Drawer>
   );
