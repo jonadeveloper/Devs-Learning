4c8b0f7881f19de8b4d29f73b477b3a97c0f152c
= <=>
2023-02-20 04:03:12 -0300

    implementamos tabla de administracion de cursos en dashboard admin. falta implementar crud

--- client/devs-learning/src/components/Dashboards/Admin/CoursesPanel.tsx
+++ client/devs-learning/src/components/Dashboards/Admin/CoursesPanel.tsx
@@ -1,43 +1,59 @@
 import React from "react";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../hooks/hooksRedux";
-//import { Link as ReactLink } from "react-router-dom";
 
 import Typography from "@mui/material/Typography";
-//import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import MUIDataTable from "mui-datatables";
-//import Avatar from "@mui/material/Avatar";
-//import Stack from "@mui/material/Stack";
 
 const CoursesPanel: React.FC = () => {
   const dispatch = useAppDispatch();
-  const columns : string[] = ["curso","precio","descipcion"];
-  const data = [
-    ["javascript","250","tatatata"],
-    ["typescript","450","tatraraatata"],
-    ["html y css","150","tataghjktata"],
-    ["react","650","tatatatgjbhba"]
-  ];
 
-  //const { courses } = useAppSelector((state) => state.courses);
-  //console.log(courses)
-  const options = {filterTypes: "checkbox"};
+  const columns = [
+    {
+      name: "name",
+      label: "Name"
+    },
+    {
+      name: "description",
+      label: "Description"
+    },
+    {
+      name: "duration",
+      label: "Duration"
+    },
+    {
+      name: "level",
+      label: "Level"
+    },
+    {
+      name: "price",
+      label: "Price"
+    },
+    {
+      name: "instructor",
+      label: "Instructor"
+    }
+  ];
+  
+
+  const { courses } = useAppSelector((state) => state.courses);
+  console.log(courses)
   useEffect(() => {});
   return (
     <Grid container xs={12}>
       <Box width="100%">
       <Typography textAlign={"center"} variant="h3">Courses</Typography>
-      <Typography textAlign={"center"} variant="h6" mt={3}>
+      <Typography textAlign={"center"} variant="h6" m={3}>
         In this section we manage all the courses on the platform
       </Typography>
       </Box>
       <MUIDataTable
-      title={"nombre"}
-      data={data}
+      title={"list of platform courses"}
+      data={courses}
       columns={columns}
-      options={options}
+      
        />
     </Grid>
   );
