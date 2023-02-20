bb9f00b006f2530421ee579db2069597f78f4139
= <=>
2023-02-20 01:30:59 -0300

    implementando tabla de administracion en panel de cursos

--- client/devs-learning/src/components/Dashboards/Admin/CoursesPanel.tsx
+++ client/devs-learning/src/components/Dashboards/Admin/CoursesPanel.tsx
@@ -1,27 +1,29 @@
 import React from "react";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../hooks/hooksRedux";
-import { Link as ReactLink } from "react-router-dom";
-import { Table } from "@mui/material";
+//import { Link as ReactLink } from "react-router-dom";
+
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
+//import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
+import MUIDataTable from "mui-datatables";
+//import Avatar from "@mui/material/Avatar";
+//import Stack from "@mui/material/Stack";
 
 const CoursesPanel: React.FC = () => {
   const dispatch = useAppDispatch();
-  const column = [
-    { 
-      title:"Course",
-      id: "id"
-    },
-    {
-      title:"Category",
-      id:"id"
-    },
-  ]
+  const columns : string[] = ["curso","precio","descipcion"];
+  const data = [
+    ["javascript","250","tatatata"],
+    ["typescript","450","tatraraatata"],
+    ["html y css","150","tataghjktata"],
+    ["react","650","tatatatgjbhba"]
+  ];
+
+  //const { courses } = useAppSelector((state) => state.courses);
+  //console.log(courses)
+  const options = {filterTypes: "checkbox"};
   useEffect(() => {});
   return (
     <Grid container xs={12}>
@@ -31,6 +33,12 @@ const CoursesPanel: React.FC = () => {
         In this section we manage all the courses on the platform
       </Typography>
       </Box>
+      <MUIDataTable
+      title={"nombre"}
+      data={data}
+      columns={columns}
+      options={options}
+       />
     </Grid>
   );
 };
