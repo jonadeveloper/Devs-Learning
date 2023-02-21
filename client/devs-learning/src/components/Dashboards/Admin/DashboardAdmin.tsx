9eef1dfb741f38d464b264b089b2574851028b6d
= <=>
2023-02-21 13:45:05 -0300

    corrigiendo errores de dashboard admin

--- client/devs-learning/src/components/Dashboards/Admin/DashboardAdmin.tsx
+++ client/devs-learning/src/components/Dashboards/Admin/DashboardAdmin.tsx
@@ -39,7 +39,7 @@ const DashboardAdmin: React.FC = () => {
   const img: string =
     "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg";
 
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
   const [content, setContent] = React.useState(0);
 
   const handleListItemClick = (
