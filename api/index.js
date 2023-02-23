5011e0c157eb47de33f896b92900d838bccdb7e3
Simon Navarrete <66700738+GoldZy11@users.noreply.github.com>
2023-02-23 20:46:09 -0300

    fixed

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const dbCategories = require("./categories.json");
 
 const PORT = 3001;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log("%s listening at", PORT);
     dbCategories.categories.map((category) => {
