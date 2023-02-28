c6e0eda95e271ac7d7b2cff2828db3c116a8e0fc
= <=>
2023-02-27 22:58:23 -0300

    agregando funcionalidad a panel de cursos en dashboard admin

--- client/devs-learning/src/components/Dashboards/Admin/CoursesPanel.tsx
+++ client/devs-learning/src/components/Dashboards/Admin/CoursesPanel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect , useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../hooks/hooksRedux";
+//import { makeStyles } from "@mui/material/";
 import { Modal , TextField , Button} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
@@ -8,12 +9,17 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import MUIDataTable, { MUIDataTableOptions, MUIDataTableColumn } from "mui-datatables";
-import { getCourses } from "../../../redux/courses/actions";
+import { getCourses , editCourseAction } from "../../../redux/courses/actions";
+
 
 const CoursesPanel: React.FC = () => {
-
+  const [modalInsertar,setModalInsertar] = useState(false);
   const { courses } = useAppSelector((state) => state.courses);
   console.log(courses)
+  const abrirCerrarModalInsertar =()=>{
+    setModalInsertar(modalInsertar);
+  }
+
 
   interface RowData{
     name: string,
@@ -26,15 +32,6 @@ const CoursesPanel: React.FC = () => {
   const dispatch = useAppDispatch();
 
  const initialData : RowData[] = courses
-//[{
-//     name: "javascript",
-//     description: "curso de javascript desde cero",
-//     duration: "15",
-//     level: "advanced",
-//     price: "2500",
-//     instructor: "Jonatan Villalva"
-// }];
-
 
 const columns: MUIDataTableColumn[] = [
   {
@@ -102,14 +99,13 @@ const columns: MUIDataTableColumn[] = [
   const [data, setData] = useState(initialData);
 
   const handleDelete = (rowIndex: number) => {
-    // Create a new array without the selected row
     const newData = [...data];
     newData.splice(rowIndex, 1);
     setData(newData);
   };
 
   const handleEdit = (rowIndex: number) => {
-    // TODO: Implement edit functionality
+
   };
   
   
@@ -117,6 +113,8 @@ const columns: MUIDataTableColumn[] = [
   useEffect(() => {
     dispatch(getCourses())
   },[dispatch]);
+
+
   return (
     <Grid container xs={12}>
       <Box width="100%">
@@ -125,13 +123,14 @@ const columns: MUIDataTableColumn[] = [
         In this section we manage all the courses on the platform
       </Typography>
       </Box>
-      <Button>crear nuevo curso</Button>
+      <Button onClick={abrirCerrarModalInsertar}>crear curso</Button>
       <MUIDataTable
       title={"list of platform courses"}
       data={data}
       columns={columns}
       options={options}      
        />
+       
     </Grid>
   );
 };
